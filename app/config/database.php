2cef2be4a04f653532dfa5ecec632ea6cb3bb2c7
Tyler Souza <tsouza.sf@gmail.com>
2014-07-21 13:08:45 -0700

    Update database.php

--- app/config/database.php
+++ app/config/database.php
@@ -57,7 +57,7 @@ return array(
 			'host'      => 'localhost',
 			'database'  => 'microsite-event',
 			'username'  => 'root',
-			'password'  => 'ty12rays',
+			'password'  => '',
 			'charset'   => 'utf8',
 			'collation' => 'utf8_unicode_ci',
 			'prefix'    => '',
